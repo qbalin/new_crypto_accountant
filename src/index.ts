590c6a2fae826ba477a64d0d2d9c79ecbb8233ce
Quentin Balin <quentin.balin@gusto.com>
2021-11-07 09:41:13 -0500

    Describe high level approach

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,30 @@
-import fs from 'fs';
-import Config from './config';
+/*
+# Goal
 
-const c = Config.parse();
-console.log(c.blockchainAccounts);
+We want to be able to output Atomic Legder Entries for each
+crypto account, in the following format:
 
-const file = fs.readFileSync('./.gitignore', { encoding: 'utf8' });
-console.log(file);
-let helloWorld = "Hello World";
+```json
+// AtomicLedgerEntry
+{
+    from: [AtomicAccount],
+    to: [AtomicAccount],
+    timestamp,
+    amount,
+}
+```
+
+An Atomic Account refers to an centralized or decentralized
+account and holding a single kind of currency.
+
+Once we have Atomic Ledger Entries, it should be easy to
+create time series, aggregate them by currency or get
+their equivalent price in fiat, etc.
+
+# Steps
+
+Do achieve this, we will need to:
+
+1. Pull the data archive from all accounts and output it in a normalized way
+2. Process the data to output Atomic Ledger Entries
+*/
