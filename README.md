5bd2fcd0fc6ba319db61bb707374907e24f0f8b3
Quentin Balin <quentin.balin@gusto.com>
2021-11-07 10:25:18 -0500

    Add eslint and a README

---
+++ README.md
@@ -0,0 +1,41 @@
+# Development Setup
+
+
+## IDE setup
+
+1. It's recommended to use [Visual Studio Code](https://code.visualstudio.com/), as it has a good
+support for TypeScript autocompletion and linting.
+2. Install the [ESLint pluggin](https://marketplace.visualstudio.com/items?itemName=dbaeumer.vscode-eslint)
+for VSCode
+
+
+## Development tools setup
+
+1. Install [Node.js](https://nodejs.org/en/) on your machine if you don't have it
+2. Install [Yarn.js](https://classic.yarnpkg.com/en/docs/getting-started)
+3. Install the dependencies: in the code root directory, run `yarn install`
+4. In VSCode, Using Ctrl+Shift+P or Cmd+Shift+P, open `Preferences: Open Settings (JSON)` and add the following entries:
+```
+    "files.trimTrailingWhitespace": true,
+    "eslint.validate": ["typescript"],
+    "editor.codeActionsOnSave": {
+        "source.fixAll.eslint": true
+    }
+```
+5. Restart VSCode
+
+
+## Development
+
+### Watch mode
+
+In VSCode, in the top-left of the left-nav, click the three dots in front of "EXPLORER" and tick
+"NPM Scripts" if it's not already.
+
+Unfold "NPM Scripts" and click the play button in front of the "start" script. This will start the
+TypeScript compiler in watch mode, so that each change to a file is immediately compiled.
+
+
+### Running the code
+To run the code, simply execute `node .` in a console.
+
--- src/config.ts
+++ src/config.ts
@@ -23,45 +23,45 @@ interface Configuration {
 }
 
 class Config {
-    exchangeAccounts: Array<ExchangeAccountConfig>;
-    blockchainAccounts: Array<BlockchainAccountConfig>;
+    exchangeAccounts: Array<ExchangeAccountConfig>
+
+    blockchainAccounts: Array<BlockchainAccountConfig>
 
     constructor({ exchangeAccounts, blockchainAccounts }: Configuration) {
-        this.exchangeAccounts = exchangeAccounts;
-        this.blockchainAccounts = blockchainAccounts;
+      this.exchangeAccounts = exchangeAccounts;
+      this.blockchainAccounts = blockchainAccounts;
     }
 
     static parse() : Config {
-        if (!fs.existsSync(configFilePath)) {
-            console.info(`Config file not found at ${configFilePath}. Creating one.`);
-            const content = {
-                exchangeAccounts: [
-                    {
-                        exchangeName: 'Coinbase or KuCoin or Binance, etc..',
-                        privateApiKey: 'The private API generated from your account. Read-only is enough.',
-                        nickname: 'A name to recognize your account'
-                    },
-                    {
+      if (!fs.existsSync(configFilePath)) {
+        console.info(`Config file not found at ${configFilePath}. Creating one.`);
+        const content = {
+          exchangeAccounts: [
+            {
+              exchangeName: 'Coinbase or KuCoin or Binance, etc..',
+              privateApiKey: 'The private API generated from your account. Read-only is enough.',
+              nickname: 'A name to recognize your account',
+            },
+            {
 
-                    }
-                ],
-                blockchainAccounts: [
-                    {
-                        blockchainName: 'Ethereum or Polygon, etc...',
-                        privateApiKey: 'The private API generated from your Explorer account.',
-                        walletAddress: 'Your public wallet address on the blockchain',
-                        nickname: 'A name to recognize your account',
-                    }, 
-                    {
+            },
+          ],
+          blockchainAccounts: [
+            {
+              blockchainName: 'Ethereum or Polygon, etc...',
+              privateApiKey: 'The private API generated from your Explorer account.',
+              walletAddress: 'Your public wallet address on the blockchain',
+              nickname: 'A name to recognize your account',
+            },
+            {
 
-                    }
-                ]
-            };
-            
-            fs.writeFileSync(configFilePath, JSON.stringify(content, null, 2));
-            
-        }
-        return new Config(JSON.parse(fs.readFileSync(configFilePath, { encoding: 'utf8'})));
+            },
+          ],
+        };
+
+        fs.writeFileSync(configFilePath, JSON.stringify(content, null, 2));
+      }
+      return new Config(JSON.parse(fs.readFileSync(configFilePath, { encoding: 'utf8' })));
     }
 }
 
